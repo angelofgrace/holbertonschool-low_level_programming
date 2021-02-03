63a8473aa4f2fa739f737da8ff53627fb047e340
Grace Fallon <graceleefallon@gmail.com>
2021-02-03 23:07:30 +0000

    Correcting semantic syntax error in retrn value

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 *main - Output string of text
 *
@@ -7,5 +8,5 @@
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle");
-return (0);
+return(0);
 }
