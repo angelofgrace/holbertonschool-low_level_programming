d830976b9a10b3a9b80e27da1abd4279d3cb8fff
Grace Fallon <graceleefallon@gmail.com>
2021-02-03 23:05:10 +0000

    Comment on main function for puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+*main - Output string of text
+*
+*Return: Words of wisdom for coders.
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
