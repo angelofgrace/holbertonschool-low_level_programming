5b1e5e5b55baa8d0b1b196dba72c287dfbce815d
Grace Fallon <graceleefallon@gmail.com>
2021-02-09 16:40:19 +0000

    Determind if a character is a letter

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,8 +1,14 @@
-
+/*custom putchar emulation*/
 int _putchar(char c);
 
+/*write all lowercase alphabet char*/
 void print_alphabet(void);
 
+/*write all lowercase alphabet char in ten separate lines*/
 void print_alphabet_x10(void);
 
+/*determine if a letter is lowercase*/
 int _islower(int c);
+
+/*determine if a character is a letter*/
+int _isalpha(int c);
