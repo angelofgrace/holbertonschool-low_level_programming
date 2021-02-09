8d44de6143c4cad8500f5637a624905536953753
Grace Fallon <graceleefallon@gmail.com>
2021-02-09 15:17:08 +0000

    Adjust spacing on task 1 to appease Betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,17 @@
 #include "holberton.h"
 
+/**
+*print_alphabet - use _putchar to write characters a to z
+*in ascending ASCII value
+*
+*return: void
+*/
+
 void print_alphabet(void)
 {
 char alpha;
 
-for (alpha='a'; alpha<='z'; alpha++)
+for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
 	_putchar(alpha);
 }
