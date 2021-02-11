b1864515bb35b60efc4709c02192fcee4b1da87c
Grace Fallon <graceleefallon@gmail.com>
2021-02-11 16:26:54 +0000

    Multiply two integers together

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* mul - multiplies two integers together
+* @a: any integer
+* @b: any integer
+*
+* Return: result of the multiplication
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -8,6 +8,7 @@ int _isupper(int c);
 /*check if a character is a single numerical digit*/
 int _isdigit(int c);
 
-
+/*multiplies two integers togethre*/
+int mul(int a, int b);
 #endif
 
