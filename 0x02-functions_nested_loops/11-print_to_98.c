22eca10d958f428a6eec309b2b5fa8e29d5c9c09
Grace Fallon <graceleefallon@gmail.com>
2021-02-11 02:45:24 +0000

    Add an if/else statement to account for numbers over 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
+/**
+*print_to_98 - take input and start counting until 98
+*@n: any natural number
+*
+*Return: void
+*/
+
 void print_to_98(int n)
 {
-	while (n <= 98)
+	while (n != 98)
 	{
 		printf("%d, ", n);
 
-		n++;
+		if (n < 98)
+		{
+			n++;
+		}
+		else
+		{
+			n--;
+		}
 	}
+	printf("%d", n);
 	printf("\n");
 }
