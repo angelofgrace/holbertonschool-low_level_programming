c419ae094be4092fcb787280c7d57b82220f3a19
Grace Fallon <graceleefallon@gmail.com>
2021-02-12 04:15:17 +0000

    Allow for case zero, create newline

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+* print_square - builds a square of hashtags
+* @size: define the height and width values (must be one integer)
+*
+* Return: void
+*/
+
 void print_square(int size)
 {
 
@@ -12,6 +19,11 @@ for (x = 0; x < size; x++)
 		_putchar('#');
 	}
 
+	_putchar('\n');
+}
+
+if (size == 0)
+{
 	_putchar('\n');
 }
 
