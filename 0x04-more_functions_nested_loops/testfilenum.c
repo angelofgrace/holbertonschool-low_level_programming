a937bcd7b8f90f04d1bd3315d867b501b9a3ccd0
Grace Fallon <graceleefallon@gmail.com>
2021-02-12 21:23:34 +0000

    Session save, progress on 12

---
+++ 0x04-more_functions_nested_loops/testfilenum.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdio.h>
+int main(void)
+{
+        int x;
+        printf("enter number:");
+        scanf("%i", &x);
+        print_number(x);
+        _putchar('\n');
+        return (0);
+}
