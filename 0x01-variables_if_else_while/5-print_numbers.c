c713e8410f16424ab1d0ddde5df5cae4fd44c432
Grace Fallon <graceleefallon@gmail.com>
2021-02-15 18:13:52 +0000

    The Counting Game

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - count up from zero
+*
+* Return: all numbers accounted for
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
