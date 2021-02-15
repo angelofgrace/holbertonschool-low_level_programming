5ff8e15d190a75d34fea687f5848a18d8c340b7e
Grace Fallon <graceleefallon@gmail.com>
2021-02-15 18:19:57 +0000

    Spell out each single digit number (ASCII)

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - print single digits using putchar
+*
+* Return - all numbers are spelled out
+*/
+
+int main(void)
+{
+
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c + '0'); 
+	}
+
+	putchar('\n');
+
+	return (0); 
+}
