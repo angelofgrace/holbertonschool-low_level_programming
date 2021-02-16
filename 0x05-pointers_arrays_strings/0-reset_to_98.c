67906dedbc16011cfb191997c4af067c38130088
Grace Fallon <graceleefallon@gmail.com>
2021-02-16 22:29:16 +0000

    Update the value of a pointer outside a function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* reset_to_98 reset value of target of input pointer to 98
+* @*n external pointer to an integer value
+*
+* Return: none
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
