da703e49bb1b3837f3bbb0aad3d4791bfb4efd7c
Grace Fallon <graceleefallon@gmail.com>
2021-02-16 23:49:19 +0000

    Add comment description of function and parameters

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
 
+/**
+* swap_int - swap the values of two variables outside the function
+* @a: pointer with an integer target value
+* @b: pointer with a different integer target value
+*
+* Return: void
+*/
+
 void swap_int(int *a, int *b)
 {
 	int tmp;
