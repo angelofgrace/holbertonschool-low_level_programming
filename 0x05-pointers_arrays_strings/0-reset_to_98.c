73cabbceeffdb48342f5d25841cb26005e25db74
Grace Fallon <graceleefallon@gmail.com>
2021-02-16 22:30:41 +0000

    Add delimiters for betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
-* reset_to_98 reset value of target of input pointer to 98
-* @*n external pointer to an integer value
+* reset_to_98 - reset value of target of input pointer to 98
+* @*n: external pointer to an integer value
 *
 * Return: none
 */
