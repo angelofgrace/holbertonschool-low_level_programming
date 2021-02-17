9b7dc97105af344988827baec28ad8dec7ce14c5
Grace Fallon <graceleefallon@gmail.com>
2021-02-17 00:15:03 +0000

    Find the length of an input character string

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,9 +1,14 @@
 #ifndef Holberton_h
 #define Holberton_h
 
+/** Task 0 - reset an external pointer's target value to 98*/
 void reset_to_98(int *n);
 
+/** Task 1 - Swap the value of two external variables*/
+void swap_int(int *a, int *b);
 
+/** Task 2 - Output the length of input string*/
+int _strlen(char *s);
 
 
 
