c215b29ce9eed2c59f44d5b51bc33b06356247e6
Grace Fallon <graceleefallon@gmail.com>
2021-02-17 04:02:12 +0000

    Account for greater variety of inputs, adjust logic

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
 
+int _strlen(char *s);
+
 /**
 * puts2 - print every other character of a string
 * @str: a string of any length
@@ -9,12 +11,41 @@
 
 void puts2(char *str)
 {
-	for ( ; *str != '\0'; str++)
+	int n, i;
+
+	n = _strlen(str);
+
+	for (i = 0; i <= n; i++)
 	{
-		if (*str % 2 == 0)
+		if (i % 2 == 0)
 		{
-			_putchar(*str);
+			_putchar(str[i]);
 		}
 	}
 	_putchar('\n');
+}
+
+
+
+/**
+* _strlen - find the length of a string of characters
+* @s: a string of characters in an array
+*
+* Return: the number of characters in input string (minus null val), as integer
+*/
+
+
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		x++;
+	}
+
+return (x);
 }
