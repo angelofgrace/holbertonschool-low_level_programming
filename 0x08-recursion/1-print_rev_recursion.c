35c787dba075dda4fe4288eb17cd1b82bdf004c3
Grace Fallon <graceleefallon@gmail.com>
2021-02-24 22:31:45 +0000

    Add Documentation for betty

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
-#include <stdio.h>
+
+/**
+* _print_rev_recursion - print a string in reverse
+* @s: a string literal of any size
+*
+* Return: void
+*/
 
 void _print_rev_recursion(char *s)
 {
