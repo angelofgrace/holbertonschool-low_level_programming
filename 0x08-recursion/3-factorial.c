e8ff6573ee1bf81cb5b5c77c5e63dbd7b232c601
Grace Fallon <graceleefallon@gmail.com>
2021-02-24 23:07:28 +0000

    Documentation for betty

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+* factorial - caluclate the factorial of a number
+* @n: a positive integer
+*
+* Return: result of the factorial of n , or -1 for error
+*	if value is below zero
+*/
 int factorial(int n)
 {
 	if (n <= -1)
