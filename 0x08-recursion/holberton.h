7c24f6f9a5f627407bf419769f35e10883577e61
Grace Fallon <graceleefallon@gmail.com>
2021-02-24 22:30:19 +0000

    Print a string in reverse

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -7,6 +7,8 @@ int _putchar(char c);
 /** Task 0 - Print a string, followed by a newline. */
 void _puts_recursion(char *s);
 
+/** Task 1 - Print a string in reverse */
+void _print_rev_recursion(char *s);
 
 
 
