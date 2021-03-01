e13039ac1db1257e680dcd74a3f2a523cb635e8b
Grace Fallon <graceleefallon@gmail.com>
2021-03-01 01:39:08 +0000

    Multiple two numbers input on the command line

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc < 2)
+	{
+		printf("Error");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", (num1 * num2));
+	return (0);
+	
+
+}
