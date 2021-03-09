bb27f3611012f8bb7a3590b293246b29ac23be09
Grace Fallon <graceleefallon@gmail.com>
2021-03-09 03:48:01 +0000

    Initial commit for Structs/typedef, sample struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+Holberton Foundations Structures and Typedef
+
+Created and Compiled by Grace Fallon
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - the goodest dogs
+* @name: who's a good dog
+* @age: you've always been a good dog
+* @owner: you're my bestest dog, aren't you
+*
+* Description: truly, good dogs
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+/** DOG_H endif */
+#endif
