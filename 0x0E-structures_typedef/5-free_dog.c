2a317b978af537e391b860678dfcf907a7528b31
Grace Fallon <graceleefallon@gmail.com>
2021-03-09 19:34:49 +0000

    Documentation and account for NULL input

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,9 +2,19 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+* free_dog - free all elements of struct dog and alloc'd memory for struct dog
+* @d: pointer to struct dog
+*
+* Return: void
+*/
+
 void free_dog(dog_t *d)
 {
+	if (d)
+	{
 	free((*d).name);
 	free((*d).owner);
 	free(d);
+	}
 }
