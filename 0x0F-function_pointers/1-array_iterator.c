1547d4510fd0b75e81887ac86c9a5475037c7475
Grace Fallon <graceleefallon@gmail.com>
2021-03-10 17:45:53 +0000

    Apply a function to each element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
