e87e90d5c4d3f1b02c09c917483c0ffcc57c0684
Grace Fallon <graceleefallon@gmail.com>
2021-03-10 19:05:17 +0000

    Account for NULL array and NULL function pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,6 +16,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (array == NULL || action == NULL)
+	{
+		exit(0);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
