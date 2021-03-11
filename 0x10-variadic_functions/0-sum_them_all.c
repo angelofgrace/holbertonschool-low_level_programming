04dd2af41a255d3e5a919791c5098b90b9f880cd
Grace Fallon <graceleefallon@gmail.com>
2021-03-11 21:09:30 +0000

    Documentation and cleanup for betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,12 @@
 #include "variadic_functions.h"
 
+/**
+* sum_them_all - find the sum of any number of integers
+* @n: number of arguments to follow
+*
+* Return: integer sum of all input integers
+*/
+
 int sum_them_all(const unsigned int n, ...)
 {
 
@@ -16,10 +23,10 @@ int sum_them_all(const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		/** increase sum by current argument value */
-		sum += va_arg (nums, int); 
+		sum += va_arg(nums, int);
 	}
 
-	va_end (nums);
+	va_end(nums);
 	return (sum);
 
 }
