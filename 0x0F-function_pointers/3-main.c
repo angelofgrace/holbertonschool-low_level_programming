f3d9a04790a56d8ee6cf83932b70db9276347f91
Grace Fallon <graceleefallon@gmail.com>
2021-03-11 03:49:53 +0000

    Account for inconsistent operators, move /0 and %0 cases to op_func

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,17 +26,17 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if (b == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
+	
 	printf("%i\n", get_op_func(argv[2])(a, b));
 
 	return (0);
