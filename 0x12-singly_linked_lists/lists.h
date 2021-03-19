a5ba92e360771b890021ed771a87bd20b986f45a
Grace Fallon <graceleefallon@gmail.com>
2021-03-19 02:21:42 +0000

    Fix documentation typo for betty

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,7 +8,7 @@
 * struct list_s - singly linked list
 * @str: string - (malloc'ed string)
 * @len: length of the string
-* @nextL points to the next node
+* @next: points to the next node
 *
 * Description: singly linked list node structure
 * for Holberton project
