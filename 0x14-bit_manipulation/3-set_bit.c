411319946e421f14bd6266c5775632fbae67551b
Grace Fallon <graceleefallon@gmail.com>
2021-03-26 16:34:19 +0000

    Documentation for betty

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+* set_bit - Set a specific bit value within an unsigned int
+* @n: pointer to an unsigned long int
+* @index: specific bit to be set
+*
+* Return: 1 if success, -1 if failure
+*/
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
