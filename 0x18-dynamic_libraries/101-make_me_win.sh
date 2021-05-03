c9d700230a0c69f5ef3ab1466374ab760d498558
Grace Fallon <graceleefallon@gmail.com>
2021-05-03 04:52:26 +0000

    no exporty

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -O- ://github.com/angelofgrace/holbertonschool-low_level_programming/blob/2d07ccc642a88c6579199bd72a3c09f64ec21e5e/0x18-dynamic_libraries/printwin.so 
-export LD_PRELOAD=./printwin.so
+LD_PRELOAD=./printwin.so
