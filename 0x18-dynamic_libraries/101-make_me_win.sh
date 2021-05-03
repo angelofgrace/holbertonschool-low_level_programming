b87d70af5501f10533b87b8aab45da58c06d6dc6
Grace Fallon <graceleefallon@gmail.com>
2021-05-03 04:50:01 +0000

    better or worse?

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -o- ://github.com/angelofgrace/holbertonschool-low_level_programming/blob/2d07ccc642a88c6579199bd72a3c09f64ec21e5e/0x18-dynamic_libraries/printwin.so 
+wget ://github.com/angelofgrace/holbertonschool-low_level_programming/blob/2d07ccc642a88c6579199bd72a3c09f64ec21e5e/0x18-dynamic_libraries/printwin.so 
 LD_PRELOAD=./printwin.so
