7bd235305c55feb062aa7bcd8fbb2a294691fa73
Grace Fallon <graceleefallon@gmail.com>
2021-09-03 18:10:13 -0500

    print every search, before return;  betty comment

--- 0x02-search_algorithms/0-linear.c
+++ 0x02-search_algorithms/0-linear.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stlib.h>
 
+/**
+ * linear_search - Evaluate each element for equality until true
+ * 
+ * @array: pointer to first element of given array
+ * @size: length of the array
+ * @value: sought value
+ * 
+ * Return: index of found value, or -1
+ */
 int linear_search(int *array, size_t size, int value)
 {
     int x;
@@ -11,10 +20,10 @@ int linear_search(int *array, size_t size, int value)
 
     for (x = 0; x < size; x++)
     {
+        printf("Value checked array[%d] = [%d]", x, value);
         if (array[x] == value)
-        {
+        { 
             return (x);
-            printf("Value checked array[%d] = [%d]", x, value);
         }
     }
 
